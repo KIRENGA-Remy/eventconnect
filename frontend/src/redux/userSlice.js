b8404999b1c4822fcf56d37e4a39ed510c8ac9d1
GITOLI Remy Claudien <gitoliremy@gmail.com>
2024-07-05 16:59:51 +0200

    expectation of function on userslice solved

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -17,19 +17,19 @@ export const userSlice = createSlice({
     loginRedux: (state, action) => {
       console.log(action.payload.data);
       // state.user = action.payload.data;  // Use action.payload directly
-      state.fullName = action.payload.data.fullName,
-      state.email = action.payload.data.email,
-      state.phoneNumber = action.payload.data.phoneNumber,
-      state.userprofile = action.payload.data.userprofile,
-      state.id = action.payload.data.id,
+      state.fullName = action.payload.data.fullName
+      state.email = action.payload.data.email
+      state.phoneNumber = action.payload.data.phoneNumber
+      state.userprofile = action.payload.data.userprofile
+      state.id = action.payload.data.id
       state.username = action.payload.data.username
     },
     logoutRedux: (state) => {
-      state.fullName = "",
-      state.email = "",
-      state.phoneNumber = "",
-      state.userprofile = "",
-      state.id = "",
+      state.fullName = ""
+      state.email = ""
+      state.phoneNumber = ""
+      state.userprofile = ""
+      state.id = ""
       state.username = ""
     },
   },
