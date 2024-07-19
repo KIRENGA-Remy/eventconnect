863efd54f616c61eef9cf4abc104492a3a96584f
GITOLI Remy Claudien <gitoliremy@gmail.com>
2024-07-19 20:32:08 +0200

    fa clock changes

--- frontend/src/components/EventsCard.js
+++ frontend/src/components/EventsCard.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { FaClock , FaRegCalendar} from 'react-icons/fa'
-import  image  from '../assets/arenas.jpg'
-import genz  from '../assets/genz.jpeg'
-import  gen  from '../assets/genz.jpg'
+// import  image  from '../assets/arenas.jpg'
+// import genz  from '../assets/genz.jpeg'
+// import  gen  from '../assets/genz.jpg'
 
 
 function EventsCard({name, image, category, country, startTime}) {
@@ -20,7 +20,7 @@ function EventsCard({name, image, category, country, startTime}) {
     // </div>
     <span className=' shadow-md p-4 rounded-md'><img src={image} className='w-[300px] h-[180px] object-fill my-2' />
     <div className='flex flex-row justify-between'>
-    <p><span className='font-bold text-2xl text-[#20B486]'>{name}</span></p><h1><span className='px-1 font-bold'>At</span>{startTime}</h1>
+    <p><span className='font-bold text-2xl text-[#20B486]'>{name}</span></p><h1><span className='px-1 font-bold'><FaClock />At</span>{startTime}</h1>
     </div>
     <div className='flex flex-row justify-between py-2'>
       <div className='flex flex-col'>
