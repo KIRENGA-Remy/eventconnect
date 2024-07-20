5eb0c830abb27b5d3deeeaf07241b155fd769294
GITOLI Remy Claudien <gitoliremy@gmail.com>
2024-07-20 20:38:40 +0200

    chnages about dynamic using of events

--- frontend/src/components/EventsCard.js
+++ frontend/src/components/EventsCard.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { FaClock , FaRegCalendar} from 'react-icons/fa'
+import Slider from 'react-slick'
 // import  image  from '../assets/arenas.jpg'
 // import genz  from '../assets/genz.jpeg'
 // import  gen  from '../assets/genz.jpg'
 
 
 function EventsCard({name, image, category, country, startTime}) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000
+  };
   return (
     // <div className='bg-white shadow-md p-2 rounded'>
     //   <div className='absolute gap-8 mx-auto w-[240px] h-[180px]'>
@@ -18,7 +28,12 @@ function EventsCard({name, image, category, country, startTime}) {
     //   </div>
     //   <h3>{country}</h3>
     // </div>
-    <span className=' shadow-md p-4 rounded-md'><img src={image} className='w-[340px] h-[220px] object-fill my-2' />
+    <Slider {...settings} className=''>
+    <span className=' shadow-md p-4 rounded-md'>
+    <div className='relative'>
+          <img src={image} className='w-[300px] h-[180px] object-fill my-2' />
+          <span className='absolute bottom-0 right-0 w-max h-max z-10 bg-green-500 text-white p-2 rounded-t-md rounded-tr-none font-semibold'>Upcoming</span>
+    </div>
     <div className='flex flex-row justify-between'>
     <p><span className='font-bold text-2xl text-[#20B486]'>{name}</span></p><h1><span className='px-1 font-bold '>At</span>{startTime}</h1>
     </div>
@@ -29,7 +44,9 @@ function EventsCard({name, image, category, country, startTime}) {
     </div>
     <p className='py-3 px-4  rounded-md bg-[#20B486] hover:bg-white border hover:border-[#20B486] hover:text-[#20B486] text-white font-bold cursor-pointer'>Book</p>
     </div>
+    <p className='bg-blue-600 hover:bg-blue-700 w-full p-2 text-white flex justify-center'>Buy Ticket</p>
     </span>
+    </Slider>
   )
 }
 
--- frontend/src/layout/Landing.js
+++ frontend/src/layout/Landing.js
@@ -2,13 +2,24 @@ import React from 'react';
 import { Hero, Navbar, Companies, Courses, Achievement, Categories, Feedback, CTA, Footer } from '../components';
 import EventsCard from '../components/EventsCard';
 import { useSelector } from 'react-redux';
-// import  image  from '../assets/arenas.jpg'
-// import genz  from '../assets/genz.jpeg'
-// import  gen  from '../assets/genz.jpg'
+import  image  from '../assets/arenas.jpg'
+import genz  from '../assets/genz.jpeg'
+import  gen  from '../assets/genz.jpg'
+import Slider from 'react-slick';
 
 export default function Landing() {
   const eventData = useSelector((state) => state.event.eventList)
-  const homeEventsCardList = eventData.slice(0, 6)
+  const homeEventsCardList = eventData.slice(1, 4)
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000
+  };
   return (
     <div>
       <Navbar />
@@ -16,7 +27,11 @@ export default function Landing() {
         <Hero />
         <Companies />
         {/* <div className=' flex justify-center w-full h-full flex-col md:flex-row gap-8 cursor-pointer'>
-        <span className=' shadow-md p-4 rounded-md'><img src={genz} className='w-[300px] h-[180px] object-fill my-2' />
+        <span className=' shadow-md p-4 rounded-md'>
+          <div className='relative'>
+          <img src={genz} className='w-[300px] h-[180px] object-fill my-2' />
+          <span className='absolute bottom-0 right-0 w-max h-max z-10 bg-green-500 text-white p-2 rounded-t-md rounded-tr-none font-semibold'>Upcoming</span>
+          </div>
           <div className='flex flex-row justify-between'>
           <p><span className='font-bold text-2xl text-[#20B486]'>Argentina vs Colombia</span></p><h1><span className='px-1 font-bold'>At</span>12:00</h1>
           </div>
@@ -27,8 +42,13 @@ export default function Landing() {
           </div>
           <p className='py-3 px-4  rounded-md bg-[#20B486] hover:bg-white border hover:border-[#20B486] hover:text-[#20B486] text-white font-bold cursor-pointer'>Book</p>
           </div>
+          <p className='bg-blue-600 hover:bg-blue-700 w-full p-2 text-white flex justify-center font-semibold'>Buy Ticket</p>
           </span>
-          <span className=' shadow-md p-4 rounded-md'><img src={image} className='w-[300px] h-[180px] object-fill my-2' />
+          <span className=' shadow-md p-4 rounded-md'>
+          <div className='relative'>
+          <img src={image} className='w-[300px] h-[180px] object-fill my-2' />
+          <span className='absolute bottom-0 right-0 w-max h-max z-10 bg-green-500 text-white p-2 rounded-t-md rounded-tr-none font-semibold'>Upcoming</span>
+          </div>
           <div className='flex flex-row justify-between'>
           <p><span className='font-bold text-2xl text-[#20B486]'>Argentina vs Colombia</span></p><h1><span className='px-1 font-bold'>At</span>12:00</h1>
           </div>
@@ -39,8 +59,13 @@ export default function Landing() {
           </div>
           <p className='py-3 px-4  rounded-md bg-[#20B486] hover:bg-white border hover:border-[#20B486] hover:text-[#20B486] text-white font-bold cursor-pointer'>Book</p>
           </div>
+          <p className='bg-blue-600 hover:bg-blue-700 w-full p-2 text-white flex justify-center font-semibold'>Buy Ticket</p>
           </span>
-          <span className=' shadow-md p-4 rounded-md'><img src={gen} className='w-[300px] h-[180px] object-fill my-2' />
+          <span className=' shadow-md p-4 rounded-md'>
+          <div className='relative'>
+          <img src={gen} className='w-[300px] h-[180px] object-fill my-2' />
+          <span className='absolute bottom-0 right-0 w-max h-max z-10 bg-green-500 text-white p-2 rounded-t-md rounded-tr-none font-semibold'>Upcoming</span>
+          </div>
           <div className='flex flex-row justify-between'>
           <p><span className='font-bold text-2xl text-[#20B486]'>Argentina vs Colombia</span></p><h1><span className='px-1 font-bold'>At</span>12:00</h1>
           </div>
@@ -51,7 +76,9 @@ export default function Landing() {
           </div>
           <p className='py-3 px-4  rounded-md bg-[#20B486] hover:bg-white border hover:border-[#20B486] hover:text-[#20B486] text-white font-bold cursor-pointer'>Book</p>
           </div>
+          <p className='bg-blue-600 hover:bg-blue-700 w-full p-2 text-white flex justify-center font-semibold'>Buy Ticket</p>
           </span>
+          
       </div> */}
         {/* <Courses /> */}
 
