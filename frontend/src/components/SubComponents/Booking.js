5df40cb4647964bdfa3e32f4079d48f37d50f38e
GITOLI Remy Claudien <gitoliremy@gmail.com>
2024-07-26 00:50:38 +0200

    booking frontendjs modif

--- frontend/src/components/SubComponents/Booking.js
+++ frontend/src/components/SubComponents/Booking.js
@@ -1,101 +1,149 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { toast } from "react-hot-toast";
+import { toast } from 'react-hot-toast';
 
-function Booking({eventDisplay}) {
-    const {price, eventname} = eventDisplay;
-    const [credentials, setCredentials] = useState({
-        userId:'',
-        userEmail:'',
-        eventName:'',
-        fullName:'',
-        guestSize:'',
-        phone:'',
-        bookAt:''
-    });
-    const navigate = useNavigate();
-    const handleChange = e => {
-        setCredentials(prev =>({...prev, [e.target.id]:e.target.value}))
-    };
+function Booking({ eventDisplay }) {
+  const { price, eventName } = eventDisplay;
+  const [credentials, setCredentials] = useState({
+    userId: '',
+    userEmail: '',
+    eventName: '',
+    fullName: '',
+    guestSize: '',
+    phone: '',
+    bookAt: ''
+  });
+  const navigate = useNavigate();
 
-    const handleClick = async (e) => {
-        e.preventDefault();
-        try {
-          const response = await fetch(`${process.env.REACT_APP_API_URL}/v1/api/booking`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              fullName: credentials.fullName,
-              userEmail: credentials.userEmail,
-              bookAt: credentials.bookAt,
-              guestSize: credentials.guestSize,
-              phone: credentials.phone,
-              eventName: eventDisplay.eventName
-            }),
-          });
-          const dataRes = await response.json();
-          if (response.status === 200) {
-            toast.success(dataRes.message);
-            navigate("/thankyou", { replace: true });
-          } else {
-            toast.error(dataRes.message);
-          }
-        } catch (error) {
-          console.error(error);
-          toast.error("Booking event failed. Network issues, Please try again.");
-        }
-      };
-    
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
 
-    const serviceFee = 10
-    const multiplePrice = Number(price) * Number(credentials.guestSize)
-    const totalAmount = Number(multiplePrice) + Number(serviceFee)
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/v1/api/booking`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          fullName: credentials.fullName,
+          userEmail: credentials.userEmail,
+          bookAt: credentials.bookAt,
+          guestSize: credentials.guestSize,
+          phone: credentials.phone,
+          eventName: eventName
+        })
+      });
+      const dataRes = await response.json();
+      if (response.status === 200) {
+        toast.success(dataRes.message);
+        navigate('/thankyou', { replace: true });
+      } else {
+        toast.error(dataRes.message);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error('Booking event failed. Network issues, Please try again.');
+    }
+  };
+
+  const serviceFee = 10;
+  const multiplePrice = Number(price) * Number(credentials.guestSize);
+  const totalAmount = multiplePrice + serviceFee;
+
   return (
-    <div className='booking -m-4 p-4'>
-        <div className='booking_top flex items-center border-b-2 pb-8 justify-between'>
-            <h3>
-            <span className='text-3xl pr-2 font-bold'>${price}</span>/per person
-            </h3>
-            <span className='tour_rating flex items-center'>
-                <i>star icon</i>
-                {/* {avgRating === 0 ? null : avgRating} ({reviews?.length}) */}
-            </span>
+    <div className="booking -m-4 p-4">
+      <div className="booking_top flex items-center border-b-2 pb-8 justify-between">
+        <h3>
+          <span className="text-3xl pr-2 font-bold">${price}</span>/per person
+        </h3>
+        <span className="tour_rating flex items-center">
+          <i>star icon</i>
+          {/* {avgRating === 0 ? null : avgRating} ({reviews?.length}) */}
+        </span>
+      </div>
+      <div className="booking_form pt-8">
+        <h5 className="font-bold text-2xl pb-4">Information</h5>
+        <form className="booking_info-form border p-8 flex flex-col gap-8" onSubmit={handleClick}>
+          <input
+            type="text"
+            name="fullName"
+            value={credentials.fullName}
+            className="border border-t-0 border-l-0 border-r-0 w-full pb-2"
+            placeholder="Enter your full names"
+            id="fullName"
+            required
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            name="userEmail"
+            value={credentials.userEmail}
+            className="border border-t-0 border-l-0 border-r-0 w-full pb-2"
+            placeholder="Enter your email..."
+            id="userEmail"
+            required
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            name="phone"
+            value={credentials.phone}
+            className="border border-t-0 border-l-0 border-r-0 w-full pb-2"
+            placeholder="Phone number"
+            id="phone"
+            required
+            onChange={handleChange}
+          />
+          <div className="flex items-center gap-3">
+            <input
+              type="date"
+              name="bookAt"
+              value={credentials.bookAt}
+              className="border border-t-0 border-l-0 border-r-0 w-full pb-2"
+              id="bookAt"
+              required
+              onChange={handleChange}
+            />
+            <input
+              type="number"
+              name="guestSize"
+              value={credentials.guestSize}
+              className="border border-t-0 border-l-0 border-r-0 w-full pb-2"
+              placeholder="Guest size"
+              id="guestSize"
+              required
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            type="submit"
+            className="px-4 py-2 mx-2 rounded-3xl bg-[#20B486] hover:bg-white border hover:border-[#20B486] hover:text-[#20B486] text-white font-bold"
+          >
+            Book Now
+          </button>
+        </form>
+      </div>
+      <div className="flex flex-col">
+        <div className="flex flex-row justify-between font-semibold py-4 text-gray-600">
+          <span className="pr-2 text-lg">
+            ${price} x {credentials.guestSize} person
+          </span>
+          <span className="pr-2 text-lg">${multiplePrice}</span>
         </div>
-        <div className='booking_form pt-8'>
-            <h5 className='font-bold text-2xl pb-4'>Information</h5>
-            <form className='booking_info-form border p-8 flex flex-col gap-8' onSubmit={handleClick}>
-                <input type='text' name='fullName' value={credentials.fullName} className='border border-t-0 border-l-0 border-r-0 w-full pb-2' placeholder='Enter your full names' id='fullName' required onChange={handleChange} />
-                <input type='email' name='userEmail' value={credentials.userEmail} className='border border-t-0 border-l-0 border-r-0 w-full pb-2' placeholder='Enter your email... ' id='email' required onChange={handleChange} />
-                <input type='number' name='phone' value={credentials.phone} className='border border-t-0 border-l-0 border-r-0 w-full pb-2' placeholder='Phone number' id='phoneNumber' required onChange={handleChange} />
-                <div className='flex items-center gap-3'>
-                <input type='date' name='bookAt' value={credentials.bookAt} className='border border-t-0 border-l-0 border-r-0 w-full pb-2' placeholder='' id='event date' required onChange={handleChange} />
-                <input type='number' name='guestSize' value={credentials.guestSize} className='border border-t-0 border-l-0 border-r-0 w-full pb-2' placeholder='Guest size' id='guest size' required onChange={handleChange} />
-                </div>
-            </form>
+        <div className="flex flex-row justify-between font-semibold py-4 text-gray-600">
+          <span className="pr-2 text-lg">Service charge</span>
+          <span className="pr-2 text-lg">${serviceFee}</span>
         </div>
-        <div className='flex flex-col'>
-           <div className='flex flex-row justify-between font-semibold py-4 text-gray-600'>
-                <span className='pr-2 text-lg'>${price} x {credentials.guestSize} person</span>
-                <span className='pr-2 text-lg'>${multiplePrice}</span>
-           </div>
-           <div className='flex flex-row justify-between font-semibold py-4 text-gray-600'>
-                <span className='pr-2 text-lg'>Service charge</span>
-                <span className='pr-2 text-lg'>${serviceFee}</span>
-           </div>
-           <div className='flex flex-row justify-between font-semibold pt-4 pb-12 text-gray-900'>
-                <span className='pr-2 text-lg'>Total</span>
-                <span className='pr-2 text-lg'>${totalAmount}</span>
-           </div>
-           <button
-               type='submit'
-               className='px-4 py-2 mx-2 rounded-3xl bg-[#20B486] hover:bg-white border hover:border-[#20B486] hover:text-[#20B486] text-white font-bold' onClick={handleClick}>
-                Book Now
-               </button>
+        <div className="flex flex-row justify-between font-semibold pt-4 pb-12 text-gray-900">
+          <span className="pr-2 text-lg">Total</span>
+          <span className="pr-2 text-lg">${totalAmount}</span>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Booking
+export default Booking;
